582bc8386975f1ed4874a960f6ab9117c25234f1
Jenna Tomko <jsoccer76@gmail.com>
2022-10-03 18:42:47 -0400

    updated gcode bash script

--- gengcode.bash
+++ gengcode.bash
@@ -1,16 +1,3 @@
 #!/usr/bin/bash
 
-arguments="
---scale=fit
---tolerance=0.1
---align-x=center
---align-y=center
---area=500,500,0,0
---lift-delta-z=1
---work-z=0"
-
-#--config-file=
-#--pens=pensfile
-
-
-./gcodeplot/gcodeplot.py $arguments $1 >  output$RANDOM.gcode
+ ./gcodeplot/gcodeplot.py --config-file=./config/gcodeConfig.txt $1 > $2
